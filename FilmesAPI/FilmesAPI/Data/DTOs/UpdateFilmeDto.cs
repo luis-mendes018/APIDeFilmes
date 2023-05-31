dc2ae1175882baca020cdc41a3c256e3683efec2
Luis Mendes <lms.kracker08@gmail.com>
2023-05-31 18:33:04 -0300

    Criação da API de Filmes

---
+++ FilmesAPI/FilmesAPI/Data/DTOs/UpdateFilmeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTOs;
+
+public class UpdateFilmeDto
+{
+    
+    [Required(ErrorMessage = " O título do filme  é obrigatório!")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório!")]
+    [StringLength(50, ErrorMessage = "O campo não pode ultrapassar 50 caracteres!")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatória")]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos!")]
+    public int Duracao { get; set; }
+}
